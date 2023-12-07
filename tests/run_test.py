e82510da6698d4bfd5b1c7605025682bbef12f3f
David Klein <david@soinkleined.com>
2023-12-07 17:31:35 +0000

    new file:   tests/requirements.txt 	modified:   tests/run_test.py

--- tests/run_test.py
+++ tests/run_test.py
@@ -1,17 +1,21 @@
-import pytest
-import requests
+"""
+standard package import
+"""
 from unittest.mock import patch, MagicMock
-from datetime import datetime, timedelta, timezone
+import requests
+import pytest
 from tfl_bus_monitor import TFLBusMonitor
 
-Test_StopID = '490005432S2'
+TEST_STOPID = '490005432S2'
 
 @pytest.fixture
 def tfl_bus_monitor():
+    """pytest decorator"""
     return TFLBusMonitor()
 
 def test_get_tfl_success(tfl_bus_monitor):
-    tfl_id = Test_StopID
+    """test get_tfl success"""
+    tfl_id = TEST_STOPID
     timeout = 10
     response_data = {"example_key": "example_value"}
     with patch("requests.get") as mock_get:
@@ -20,23 +24,27 @@ def test_get_tfl_success(tfl_bus_monitor):
     assert result == response_data
 
 def test_get_tfl_retry_on_connection_error(tfl_bus_monitor):
-    tfl_id = Test_StopID
+    """test get_tfl fail"""
+    tfl_id = TEST_STOPID
     timeout = 10
     with patch("requests.get") as mock_get:
-        mock_get.side_effect = [requests.exceptions.ConnectionError, MagicMock(status_code=200, json=lambda: {})]
+        mock_get.side_effect = [requests.exceptions.ConnectionError,
+                               MagicMock(status_code=200,
+                               json=lambda: {})]
         result = tfl_bus_monitor.get_tfl(tfl_id, timeout)
     assert result == {}
 
 def test_get_stop_name_cache(tfl_bus_monitor):
-    stop_id = Test_StopID
+    """test get_stop_name_cache"""
+    stop_id = TEST_STOPID
     tfl_bus_monitor.stop_name_cache[stop_id] = "Cached Stop Name"
     result = tfl_bus_monitor.get_stop_name(stop_id)
     assert result == "Cached Stop Name"
 
 def test_get_stop_name_request(tfl_bus_monitor):
-    stop_id = Test_StopID
+    """test get_stop_name request"""
+    stop_id = TEST_STOPID
     response_data = {"commonName": "Example Stop Name"}
     with patch.object(tfl_bus_monitor, "get_tfl", return_value=response_data):
         result = tfl_bus_monitor.get_stop_name(stop_id)
     assert result == "Example Stop Name"
-
